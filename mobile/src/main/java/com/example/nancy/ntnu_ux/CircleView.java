eccf5e61a176b3b2c571c2c4aa9c6f8e6516fc7b
jasoncheng <jasoncheng@zhengshijiedeMacBook-Pro.local>
2015-05-08 00:46:38 +0800

    update

--- mobile/src/main/java/com/example/nancy/ntnu_ux/CircleView.java
+++ mobile/src/main/java/com/example/nancy/ntnu_ux/CircleView.java
@@ -1,7 +1,58 @@
 package com.example.nancy.ntnu_ux;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
 /**
  * Created by jasoncheng on 15/5/7.
  */
-public class CircleView {
+public class CircleView extends View {
+
+  private int minRadius = 30;
+  private int maxRadius = 80;
+
+  public CircleView(Context ctx) {
+    super(ctx);
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+
+    Paint paint = new Paint();
+    paint.setStyle(Paint.Style.FILL);
+    paint.setColor(Color.WHITE);
+    canvas.drawPaint(paint);
+
+    // 畫圈圈
+    int radius = getRandomRedius();
+    circle(canvas, paint, getRandomX(radius), getRandomY(radius), "#99ccff", radius);
+
+    radius = getRandomRedius();
+    circle(canvas, paint, getRandomX(radius), getRandomY(radius), "#990000", radius);
+
+    radius = getRandomRedius();
+    circle(canvas, paint, getRandomX(radius), getRandomY(radius), "#00ff00", radius);
+
+  }
+
+  private int getRandomRedius(){
+    return (int) (Math.random() * (maxRadius - minRadius + 1) + minRadius);
+  }
+
+  private int getRandomX(int radius){
+    return (int) Math.ceil(Math.random() * (CircleView.this.getWidth()-radius));
+  }
+
+  private int getRandomY(int radius){
+    return (int) Math.ceil(Math.random() * (CircleView.this.getHeight()-radius));
+  }
+
+  private void circle(Canvas canvas, Paint paint, int x, int y, String color, Integer radius){
+    paint.setColor(Color.parseColor(color));
+    canvas.drawCircle(x, y, radius, paint);
+  }
 }
--- mobile/src/main/java/com/example/nancy/ntnu_ux/MainActivity.java
+++ mobile/src/main/java/com/example/nancy/ntnu_ux/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends Activity {
@@ -11,7 +12,11 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(new CircleView(this.getApplicationContext()));
+
+//    ViewGroup g = (ViewGroup)this.findViewById(R.id.parentView);
+//    g.addView(new CircleView(this.getApplicationContext(), "#ff0000", 30));
+//    g.addView(new CircleView(this.getApplicationContext(), "#99ccff", 60));
   }
 
   @Override
