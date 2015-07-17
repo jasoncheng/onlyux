b1585677176dc49d9316ab32a51456b9af816bf7
JasonCheng <17hado.com@gmail.com>
2015-07-17 22:49:05 +0800

    update

--- mobile/src/main/java/com/example/nancy/ntnu_ux/Consts.java
+++ mobile/src/main/java/com/example/nancy/ntnu_ux/Consts.java
@@ -7,9 +7,9 @@ public class Consts {
   public static final String PARSE_APP_ID = "UvIFyvUB5DsoEIVGPbRPaapK3foQxO77tf0WpbuU";
   public static final String PARSE_APP_KEY = "eNKyX8sCeWQ7GKrmpFt4MLhyBOdPAp4KmVWRJ1n6";
 
-  public static final int MAX_FAILURE = 5;
   public static final int MAX_AGE = 45;
   public static final int MIN_AGE = 6;
   public static final int[] CIRCLE_SIZE = {90, 100, 120, 130, 140};
   public static final int EACH_CIRCLE_TEST_TIMES = 5;
+  public static final int MAX_FAILURE = (3 * CIRCLE_SIZE.length * EACH_CIRCLE_TEST_TIMES)+1;
 }
